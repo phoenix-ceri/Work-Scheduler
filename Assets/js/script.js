6238c02345efe3e094df4606055b89f1f03d520a
Phoenix <Phoenixceri@protonmail.com>
2022-12-12 19:35:31 -0500

    used Prettier to make more readable

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,33 +1,36 @@
-$(function ()
-{
-//variables
+$(function () {
+  //variables
 
   //starts at 9 am
-    let start = 9;
+  let start = 9;
   //ends at 8pm
-    let end = 21;
+  let end = 21;
   //ref the main container
-    let mainContainerEl = $('.container-lg');
+  let mainContainerEl = $(".container-lg");
   //pulls the current day info
-    let dateTimeEl = $('#currentDay');
-  //sets up the date time group 
-    let [Hour, Date] = DateTime(); 
+  let dateTimeEl = $("#currentDay");
+  //sets up the date time group
+  let [Hour, Date] = DateTime();
 
   dateTimeEl.text(Date);
 
   //functions
 
   //shows the Month then the day and then the date
-  function DateTime()
-  {
+  function DateTime() {
     let newDate = dayjs();
-    return [newDate.hour(),getDay(newDate.day()) + "  The "+ getDate(newDate.date())+ " Of "+getMonth(newDate.month())];
+    return [
+      newDate.hour(),
+      getDay(newDate.day()) +
+        "  The " +
+        getDate(newDate.date()) +
+        " Of " +
+        getMonth(newDate.month()),
+    ];
   }
-  //picks the month 
-  function getMonth(month) 
-  {
-    switch (month)
-    {
+  //picks the month
+  function getMonth(month) {
+    switch (month) {
       case 0:
         month = "January";
         break;
@@ -64,14 +67,12 @@ $(function ()
       case 11:
         month = "December";
     }
-  
+
     return month;
   }
   // picks the day
-  function getDay(day) 
-  {
-    switch (day)
-    {
+  function getDay(day) {
+    switch (day) {
       case 0:
         day = "Sun.";
         break;
@@ -93,57 +94,66 @@ $(function ()
       case 6:
         day = "Sat.";
     }
-  
+
     return day;
   }
   // picks the date and adds the suffix for the corisponding date found this on stackoverflow
-  function getDate(ord) 
-  {
-    let s = ['th', 'st', 'nd', 'rd'];
+  function getDate(ord) {
+    let s = ["th", "st", "nd", "rd"];
     let v = ord % 100;
     return ord + (s[(v - 20) % 10] || s[v] || s[0]);
   }
-//allows the theme changes based on the time
-  for (let i = start; i < end; i++)
-  {
+  //allows the theme changes based on the time
+  for (let i = start; i < end; i++) {
     let hour;
     let meridiem;
     let tense;
-    meridiem = (i >= 12) ? " PM" : " AM";
-    hour = (i % 12) || 12;
-    if (i == (Hour))
-    {
+    meridiem = i >= 12 ? " PM" : " AM";
+    hour = i % 12 || 12;
+    if (i == Hour) {
       tense = "present";
-    } else if (i < (Hour))
-    {
+    } else if (i < Hour) {
       tense = "past";
-    } else
-    {
+    } else {
       tense = "future";
     }
     mainContainerEl.append(
-      $("<div>", { "id": "hour-" + hour, "class": "row time-block " + tense }).append(
-        $("<div>", { "class": "col-2 col-md-1 hour text-center py-3" }).text(hour + meridiem),
-        $("<textArea>", { "class": "col-8 col-md-10 description", "rows": "3" }),
-        $("<button>", { "class": "btn saveBtn col-2 col-md-1", "aria-label": "save" }).append(
-          $("<i>", { "class": "fas fa-save", "aria-hidden": "true" }))));
+      $("<div>", {
+        id: "hour-" + hour,
+        class: "row time-block " + tense,
+      }).append(
+        $("<div>", { class: "col-2 col-md-1 hour text-center py-3" }).text(
+          hour + meridiem
+        ),
+        $("<textArea>", { class: "col-8 col-md-10 description", rows: "3" }),
+        $("<button>", {
+          class: "btn saveBtn col-2 col-md-1",
+          "aria-label": "save",
+        }).append($("<i>", { class: "fas fa-save", "aria-hidden": "true" }))
+      )
+    );
   }
   // allows user to edit and save the information in the text field
-  mainContainerEl.on("click", ".time-block button", function (event)
-  {
-    localStorage.setItem($(this).parent().attr("id"), $(this).parent().children("textArea").val());
-    mainContainerEl.prepend($("<div>", { "class": "row entry-msg" }).append($("<p>", 
-    { "class": "col-12 message text-center text-primary lead py-2" })
-    .text("Saved")));
-    setTimeout(function ()
-    {
-      $('.message').remove();
+  mainContainerEl.on("click", ".time-block button", function (event) {
+    localStorage.setItem(
+      $(this).parent().attr("id"),
+      $(this).parent().children("textArea").val()
+    );
+    mainContainerEl.prepend(
+      $("<div>", { class: "row entry-msg" }).append(
+        $("<p>", {
+          class: "col-12 message text-center text-primary lead py-2",
+        }).text("Saved")
+      )
+    );
+    setTimeout(function () {
+      $(".message").remove();
     }, 2500);
   });
   //sets the local storage options
-  for (let i = 0; i < localStorage.length; i++)
-  {
-    $("#" + localStorage.key(i)).children("textArea").text(localStorage.getItem(localStorage.key(i)));
+  for (let i = 0; i < localStorage.length; i++) {
+    $("#" + localStorage.key(i))
+      .children("textArea")
+      .text(localStorage.getItem(localStorage.key(i)));
   }
 });
-
